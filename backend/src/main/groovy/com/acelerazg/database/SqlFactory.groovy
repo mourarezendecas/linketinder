c5c33da6c9977c0371616755f2484d61828853a7
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2022-10-15 17:28:09 -0300

    CRUD do Candidato OK e sincronização com o BD também.

--- backend/src/main/groovy/com/acelerazg/database/SqlFactory.groovy
+++ backend/src/main/groovy/com/acelerazg/database/SqlFactory.groovy
@@ -40,6 +40,138 @@ class SqlFactory {
         sql.close()
     }
 
+    static def listaCandidato(){
+        def sql = Sql.newInstance(dbConnParams)
+        sql.eachRow("SELECT * FROM candidatos"){
+            candidato ->
+                println('---ID DO CANDIDATO: ' + candidato.id_candidato + '---')
+                println('NOME: ' + candidato.nome_candidato)
+                println('DATA DE NASC.: ' + candidato.data_nascimento)
+                println('CPF: ' + candidato.cpf)
+                println('PAIS: ' + candidato.pais_candidato)
+                println('CEP: ' + candidato.cep_candidato)
+                println('EMAIL: '+ candidato.email_candidato)
+                println('DESCRIÇÃO: ' + candidato.descricao_candidato)
+                println()
+        }
+        sql.close()
+    }
+
+    static def atualizacandidato(){
+        def sql = Sql.newInstance(dbConnParams)
+        println('SELECIONE O ID DO CANDIDATO QUE DESEJA ATUALIZAR: ')
+        println()
+        sql.eachRow("SELECT * FROM candidatos"){
+            candidato ->
+                println('---ID DO CANDIDATO: ' + candidato.id_candidato + '---')
+                println('NOME: ' + candidato.nome_candidato)
+                println()
+        }
+        sql.close()
+
+        printf('ID DO CANDIDATO: ')
+        int choice = (System.in.newReader().readLine() as Integer)
+
+        println('O QUE DESEJA ATUALIZAR?')
+        println('1 - Nome')
+        println('2 - Data de nascimento')
+        println('3 - CPF')
+        println('4 - E-mail')
+        println('5 - País')
+        println('6 - CEP')
+        println('7 - Descrição')
+        println('8 - Senha')
+        int opt = (System.in.newReader().readLine() as Integer)
+
+        switch (opt)
+        {
+            case 1:
+                printf('Digite o nome correto: ')
+                String novonome = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET nome_candidato = ? WHERE id_candidato = ?' ,novonome,choice)
+                sqlupdate.close()
+                break
+            case 2:
+                printf('Digite a data de nascimento correta: ')
+                String novadata = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET data_nascimento = ? WHERE id_candidato = ?' ,novadata,choice)
+                sqlupdate.close()
+                break
+            case 3:
+                printf('Digite o cpf correto: ')
+                String novocpf = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET cpf = ? WHERE id_candidato = ?' ,novocpf,choice)
+                sqlupdate.close()
+                break
+            case 4:
+                printf('Digite o e-mail correto: ')
+                String novoemail = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET email_candidato = ? WHERE id_candidato = ?' ,novoemail,choice)
+                sqlupdate.close()
+                break
+            case 5:
+                printf('Digite o país correto: ')
+                String novopais = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET pais_candidato = ? WHERE id_candidato = ?' ,novopais,choice)
+                sqlupdate.close()
+                break
+            case 6:
+                printf('Digite o CEP correto: ')
+                String novocep = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET cep_candidato = ? WHERE id_candidato = ?' ,novocep,choice)
+                sqlupdate.close()
+                break
+            case 7:
+                printf('Digite a nova descrição: ')
+                String novadescricao = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET descricao_candidato = ? WHERE id_candidato = ?' ,novadescricao,choice)
+                sqlupdate.close()
+                break
+            case 8:
+                printf('Digite a nova senha: ')
+                String novasenha = (System.in.newReader().readLine())
+                def sqlupdate = Sql.newInstance(dbConnParams)
+                sqlupdate.execute('UPDATE candidatos SET senha_candidato = ? WHERE id_candidato = ?' ,novasenha,choice)
+                sqlupdate.close()
+                break
+        }
+
+
+    }
+
+    static def deletaCandidato(){
+        def sql = Sql.newInstance(dbConnParams)
+        println('SELECIONE O ID DO CANDIDATO QUE DESEJA DELETAR: ')
+        println()
+        sql.eachRow("SELECT * FROM candidatos"){
+            candidato ->
+                println('---ID DO CANDIDATO: ' + candidato.id_candidato + '---')
+                println('NOME: ' + candidato.nome_candidato)
+                println()
+        }
+        sql.close()
+
+        printf('ID DO CANDIDATO: ')
+        int choice = (System.in.newReader().readLine() as Integer)
+
+        def sqldelete = Sql.newInstance(dbConnParams)
+        sqldelete.execute('DELETE FROM candidatos WHERE id_candidato = ?', choice)
+        println('EXCLUIDO!')
+        sqldelete.close()
+    }
+
+    static def criaEmpresa(){
+        def sql = Sql.newInstance(dbConnParams)
+
+        sql.close()
+    }
 
 }
 
--- backend/src/main/groovy/com/acelerazg/main/Menu.groovy
+++ backend/src/main/groovy/com/acelerazg/main/Menu.groovy
@@ -6,12 +6,20 @@ import groovy.sql.Sql
 class Menu {
     def initialpage(){
         println('---LINKETINDER---')
-        println "1 - LISTAR TODOS OS CANDIDATOS"
-        println "2 - LISTAR TODAS AS EMPRESAS"
-        println "3 - CRIAR CANDIDATO"
-        println "4 - CRIAR EMPRESA"
-        println "5 - CURTIR CANDIDATOS OU EMPRESAS"
-        println "6 - SAIR"
+        println "1 - CRIAR CANDIDATO"
+        println "2 - CRIAR EMPRESA"
+        println "3 - CRIAR VAGA"
+        println "4 - LISTAR TODOS OS CANDIDATOS"
+        println "5 - LISTAR TODAS AS EMPRESAS"
+        println "6 - LISTAR TODAS AS VAGAS"
+        println "7 - ATUALIZAR CANDIDATO"
+        println "8 - ATUALIZAR EMPRESA"
+        println "9 - ATUALIZAR VAGAS"
+        println "10 - DELETAR CANDIDATO"
+        println "11 - DELETAR EMPRESA"
+        println "12 - DELETAR VAGA"
+        println "13 - CURTIR CANDIDATOS OU EMPRESAS"
+        println "14 - SAIR"
         printf "Escolha a opção: "
         def opt = System.in.newReader().readLine() as Integer
         switchMenu(opt)
@@ -22,26 +30,32 @@ class Menu {
         switch(opt)
         {
             case 1:
-                println('Seleção 1')
-                initialpage()
-                break
-//            case 2:
-//                GeraCandidatoeEmpresa.listaEmpresas()
-//                menu()
-//                break
-            case 3:
+                //CADASTRAR CANDIDATO
                 println('INITIALIZING SERVER')
+                println()
                 SqlFactory.cadastracandidato()
                 initialpage()
                 break
-//            case 4:
-//                CriaEmpresa.cria()
-//                menu()
-//                break
-//            case 5:
-//                Curtidas.main()
-//                menu()
-//                break
+            case 4:
+                //LISTAR CANDIDATO
+                println('INITIALIZING SERVER')
+                println()
+                SqlFactory.listaCandidato()
+                initialpage()
+                break
+            case 7:
+                //ATUALIZAR CANDIDATO
+                println('INITIALIZING SERVER')
+                println()
+                SqlFactory.atualizacandidato()
+                initialpage()
+            case 10:
+                //DELETAR CANDIDATO
+                println ('INITIALIZING SERVER')
+                println()
+                SqlFactory.deletaCandidato()
+                initialpage()
+                break
         }
     }
 
