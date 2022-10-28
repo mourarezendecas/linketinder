a80726526955203c3bd8874f0565a30e0c4a1ec6
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2022-10-28 09:56:17 -0300

    Update SOLID

--- backend/src/main/groovy/com/acelerazg/classes/Candidato.groovy
+++ backend/src/main/groovy/com/acelerazg/classes/Candidato.groovy
@@ -2,19 +2,4 @@ package com.acelerazg.classes
 
 class Candidato {
     String nome, email, pais, cep, descricao, senha, cpf, data_nasc
-
-
-    @Override
-    public String toString() {
-        return "Candidato{" +
-                "nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", pais='" + pais + '\'' +
-                ", cep='" + cep + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", senha='" + senha + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", data_nasc=" + data_nasc +
-                '}';
-    }
 }
